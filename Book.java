91ff6961dfd309f2d1b85f4577fde526af7b705b
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-07-13 12:55:27 -0300

    //init repo

---
+++ Book.java
@@ -0,0 +1,8 @@
+//variables
+
+public class Book {
+    public static void main(String[] args){
+        String book = "The pragmatic programmer";
+        String publicationYear = "October 1999";
+    }
+}
---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // specify the number of Fibonacci numbers to generate
+        int[] fibonacci = new int[n];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        for (int number : fibonacci) {
+            System.out.println(number);
+        }
+    }
+}
